a085f669c312416cb18419e22b9442b4ca39dcf4
Roderick Harvey <Harveyr@roderickharvey10gmail.onmicrosoft.com>
2026-05-26 22:06:02 -0400

    Project 5 - Remote state configured in Azure Blob Storage

--- Project5-Terraform-IaC/providers.tf
+++ Project5-Terraform-IaC/providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "my-dev-ops-project"
+    storage_account_name = "nbatfstate"
+    container_name       = "tfstate"
+    key                  = "project5.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
